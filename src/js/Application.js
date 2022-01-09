e05e1f37647e0c7af82dce3a45c36b332f321292
kalin0l <aleksiev2494@gmail.com>
2022-01-09 19:09:47 +0200

    w

--- src/js/Application.js
+++ src/js/Application.js
@@ -11,14 +11,16 @@ export default class Application extends EventEmitter {
   constructor() {
     super();
 
-    const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
+    const lyrics = ["Ah","ha","ha","ha","stayin' alive","stayin' alive"];
     let count = 0;
+    
 
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = "Ah";
-
-    document.querySelector(".main").appendChild(message);
+    for(count;count < lyrics.length - 1;count++){
+     const message = document.createElement("div");
+      message.classList.add("message");
+      message.innerText = lyrics[count];
+      document.querySelector(".main").appendChild(message);
+    }
 
     this.emit(Application.events.READY);
     this._beat = Beat.events.BIT;
